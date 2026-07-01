438c5af2bf7e217db3bf4655d6a4cc37836ce5af
mmt3d <78088314+mmt3d@users.noreply.github.com>
2026-07-01 11:10:59 +0900

    fix

--- .github/scripts/generate_index.py
+++ .github/scripts/generate_index.py
@@ -1,18 +1,43 @@
 import os
 import json
+from jinja2 import Template
 
 
 metadata_dir = 'metadata'
 index_data = {'version': 'v1', 'blocklist': [], 'data': []}
+addons = {}
+
+def parse_version(version: str):
+    try:
+        return tuple(map(int, version.split('.')))
+    except ValueError:
+        return 0, 0, 0
 
 if os.path.exists(metadata_dir):
     json_files = sorted([f for f in os.listdir(metadata_dir) if f.endswith('.json')])
     for json_file in json_files:
         with open(os.path.join(metadata_dir, json_file), 'r', encoding='utf-8') as f:
             manifest = json.load(f)
-        index_data['data'] = [ext for ext in index_data['data'] if ext['id'] != manifest['id']]
-        index_data['data'].append(manifest)
+        addon_id = manifest['id']
+        if addon_id not in addons:
+            addons[addon_id] = manifest
+        else:
+            latest_version = addons[addon_id]['version']
+            if parse_version(manifest['version']) > parse_version(latest_version['version']):
+                addons[addon_id] = manifest
+    index_data['data'] = [
+        {k: v for k, v in addon.items() if k != 'extra'}
+        for addon in addons.values()
+    ]
 
 with open('index.json', 'w', encoding='utf-8') as f:
     json.dump(index_data, f, indent=2, ensure_ascii=False)
 print('index.json を再構築しました。')
+
+with open('index.j2'):
+    templ = Template(f.read())
+html = templ.render(addons=addons.values())
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(html)
+print('index.html を再構築しました。')
+
